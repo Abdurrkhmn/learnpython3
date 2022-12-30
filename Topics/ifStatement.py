6894952f81dd7565128aa98577e7d38f123e2252
Abdurrkhmn <Abdurrahmansy25@gmail.com>
2022-12-30 22:11:01 +0300

    memme

---
+++ Topics/ifStatement.py
@@ -0,0 +1,13 @@
+# if Statement
+
+test_value = 100
+
+if test_value > 1:
+  # Expression evaluates to True
+  print("This code is executed!")
+
+if test_value > 1000:
+  # Expression evaluates to False (use <)
+  print("This code is NOT executed!")
+
+print("Program continues at this point.")
