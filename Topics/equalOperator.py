5a97597880c86878aba240b0412c29ac62409397
Abdurrkhmn <Abdurrahmansy25@gmail.com>
2022-12-30 21:56:16 +0300

    tambahakan

---
+++ Topics/equalOperator.py
@@ -0,0 +1,19 @@
+# Equal operator
+"""Operator yang sama, ==, 
+digunakan untuk membandingkan dua nilai, variabel, atau ekspresi untuk menentukan apakah keduanya sama."""
+
+if 'Yes' == 'Yes':
+  # evaluates to True
+  print('They are equal')
+
+if (2 > 1) == (5 < 10):
+  # evaluates to True
+  print('Both expressions give the same result')
+
+c = '2'
+d = 2
+
+if c == d:
+  print('They are equal')
+else:
+  print('They are not equal')
---
+++ Topics/orOperator.py
@@ -0,0 +1,6 @@
+True or True      # Evaluates to True
+True or False     # Evaluates to True
+False or False    # Evaluates to False
+1 < 2 or 3 < 1    # Evaluates to True
+3 < 1 or 1 > 6    # Evaluates to False
+1 == 1 or 1 < 2   # Evaluates to True
