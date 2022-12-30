a18c5e57f81b4d78cb039abb51512f406323895d
Abdurrkhmn <Abdurrahmansy25@gmail.com>
2022-12-30 22:01:12 +0300

    ganti

---
+++ Topics/NotEqualsOperator.py
@@ -0,0 +1,15 @@
+# Not Equals Operator
+
+if "Yes" != "No":
+  # evaluates to True
+  print("They are NOT equal")
+
+val1 = 10
+val2 = 20
+
+if val1 != val2:
+  print("They are NOT equal")
+
+if (10 > 1) != (10 > 1000):
+  # True != False
+  print("They are NOT equal")
